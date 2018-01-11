ff9ff2bd7bbc8047bbf3b75fbcc980bbb57d8b72
Nitin Vangara <samuelvangara@gmail.com>
2018-01-11 14:07:57 -0500

    Project Creation Code

---
+++ app/src/main/java/raptorcorp/notepad/NotepadHome.java
@@ -0,0 +1,13 @@
+package raptorcorp.notepad;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class NotepadHome extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_notepad_home);
+    }
+}
